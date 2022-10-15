5bc02597b408f018b2d92992ce72978490aa81ac
Rehan Khalid <73833579+RKPasha@users.noreply.github.com>
2022-10-15 15:12:56 +0500

    Update and Delete functionality added for tecaher

--- routes/teacher.js
+++ routes/teacher.js
@@ -4,7 +4,7 @@ const mySqlConnection = require('../connection')
 
 Router.get('/viewAll', (req, res) => {
   mySqlConnection.query(
-    'Select * from teacher join person on teacherId = Id',
+    'SELECT * FROM teacher JOIN person ON teacherId = Id WHERE isDeleted <> 1',
     (err, rows, fields) => {
       if (!err) {
         res.send(rows)
@@ -61,6 +61,46 @@ Router.post('/addTeacher', (req, res) => {
         console.log(err)
       } else {
         res.send('Values Inserted')
+      }
+    }
+  )
+})
+
+Router.put('/editTeacher', (req, res) => {
+  console.log(req.body)
+  const id = req.body.id
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const dob = new Date(req.body.dob)
+  const email = req.body.email
+  const cnic = req.body.cnic
+  const contact = req.body.contact
+
+  mySqlConnection.query(
+    "Set @gender = (Select id from lookup where category = 'GENDER' and value = '"+req.body.gender+"' ); Set @designation = (Select id from lookup where category = 'DESIGNATION' and value = '"+req.body.designation+"' ); BEGIN; UPDATE person SET firstName = ?, lastName = ?, dob = ?, email = ?, cnic = ?, contact = ?, gender = @gender WHERE id = ?; UPDATE teacher SET designation = @designation WHERE teacherId = ?; COMMIT;",
+    [firstName, lastName, dob, email, cnic, contact, id, id],
+    (err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send('Values Updated')
+      }
+    }
+  )
+})
+
+Router.put('/deleteTeacher', (req, res) => {
+  console.log(req.body)
+  const id = req.body.id
+
+  mySqlConnection.query(
+    "BEGIN; UPDATE teacher SET isDeleted = 1 WHERE teacherId = ?; COMMIT;",
+    [id],
+    (err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send('Record Deleted(dhoka)')
       }
     }
   )
